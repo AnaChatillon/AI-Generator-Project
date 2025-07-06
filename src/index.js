9088a2430b20d432b909dc7f9de7d9f0583e014a
Ana Chatillon <a_chantil@hotmail.com>
2025-07-06 20:43:32 +0100

    new Typewriter("#poem"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 function generatePoem(event) {
-    event.preventDefault();
-    
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "I never saw a wild thing",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+  poemElement.innerHTML = "Quelque chose";
 }
 
-
-
-let poemFormElement = document.querySelector("#poem-generator-form);
-poemFormElement.addEventListener('submit', generatePoem);
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
